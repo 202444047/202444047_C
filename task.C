1369423def4fef13082d978a2c2d36b6cb1489bf
202444047 <163097364+202444047@users.noreply.github.com>
2024-04-09 14:37:18 +0900

    Update task.C

--- task.C
+++ task.C
@@ -19,26 +19,7 @@ int main()
     {
         switch (m)
         {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-            printf("%d월은 31일까지 있습니다.", m);
-            break;
-
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-            printf("%d월은 30일까지 있습니다.", m);
-            break;
-
-            case 2:
-            printf("%d월은 28일까지 있습니다.", m);
-            break;
+            /*문제를 완성시키시오.*/
         }
     }
     return 0;
